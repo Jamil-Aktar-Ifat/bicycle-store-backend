0ed38ccc3faf2e6d90983f9523087737804f8dc4
ifat <i>
2024-12-09 17:29:49 +0000

    order model created

---
+++ src/app/models/order.model.ts
@@ -0,0 +1,20 @@
+import { Schema, model, Document } from "mongoose";
+
+interface IOrder extends Document {
+  email: string;
+  product: Schema.Types.ObjectId;
+  quantity: number;
+  totalPrice: number;
+}
+
+const orderSchema = new Schema<IOrder>(
+  {
+    email: { type: String, required: true },
+    product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
+    quantity: { type: Number, required: true, min: 1 },
+    totalPrice: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+export const Order = model<IOrder>("Order", orderSchema);
